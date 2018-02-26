2e5208aa8c2441292539a87ca20858c77ca9c723
hoang <bdhoang.ht@gmail.com>
2018-02-26 14:12:41 +0700

    add user router

--- server/user/user.router.js
+++ server/user/user.router.js
@@ -8,6 +8,18 @@ let response = require('../response');
 
 router.use(bodyParser.json());
 
+router.post('/user/info', function (req, res) {
+    User.findById(req.decoded.username).then(user => {
+        if (user) {
+            res.send(response(200, 'GET USER INFOR SUCCESS', user));
+        } else {
+            res.send(response(200, 'NO USER FOUND BY ID'));
+        }
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
 router.post('/user/fullinfo', function (req, res) {
     User.findOne({
         where: {username: req.decoded.username},
