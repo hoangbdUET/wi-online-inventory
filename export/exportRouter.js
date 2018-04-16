56c8d36ceb0a1eaaa62bb6b95c69e611cdcd9873
VuThuyThuy97 <vuthuy.uet@gmail.com>
2018-04-16 10:30:47 +0700

    update

--- export/exportRouter.js
+++ export/exportRouter.js
@@ -2,14 +2,14 @@ let express = require('express');
 let router = express.Router();
 let config = require('config');
 let models = require('../server/models');
-let Well = models.Well;
 let response = require('../server/response');
 const s3 = require('../server/s3');
 let exporter = require('wi-export-test');
+let curveModel = require('../server/curve/curve.model');
 
 router.post('/well', function (req, res) {
     console.log('STARTTIME', new Date());
-    exporter.exportLas2FromInventory(config.exportPath, req.body.idObjs, models, req.decoded.username, s3, function(err, result){
+    exporter.exportLas2FromInventory(config.exportPath, req.body.idObjs, models, req.decoded.username, s3, curveModel, function(err, result){
         if(err){
             res.send(response(404, err));
         } else {
