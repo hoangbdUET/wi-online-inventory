1c38d580f4ab19d976bce11b578761535ce0dd1c
dodv <dodv212@gmail.com>
2019-02-11 10:52:54 +0700

    dlis update curve type

--- server/upload/function/dlisProcessing.js
+++ server/upload/function/dlisProcessing.js
@@ -46,6 +46,16 @@ function parseDlisFile(file, userInfo){
                 //import curve to db
                 frame['CHANNELS'].forEach(async channelName => {
                     const channel = channels[obname2Str(channelName)];
+                    let _dimension = 1;
+                    let _type = 'NUMBER';
+                    if(channel['DIMENSION']){
+                        for(const x of channel['DIMENSION']){
+                            _dimension *= x;
+                        }
+                    }
+                    if(_dimension > 1){
+                        _type = 'ARRAY';
+                    }
                     let curve = {
                         name: channelName.name,
                         unit: channel['UNITS'] ? channel['UNITS'][0] : "",
@@ -53,8 +63,9 @@ function parseDlisFile(file, userInfo){
                         stopDepth: frame['INDEX-MAX'] ? frame['INDEX-MAX'][0] : "0",
                         step: frame['SPACING'] ? frame['SPACING'][0] : "0",
                         path: channel.path,
-                        dimension: channel['DIMENSION'] ? channel['DIMENSION'][0] : 1,
-                        description: channel['LONG-NAME'] ? channel['LONG-NAME'][0] : ""
+                        dimension: _dimension,
+                        description: channel['LONG-NAME'] ? channel['LONG-NAME'][0] : "",
+                        type: _type
                     }
                     dataset.curves.push(curve);
                     //console.log("==> CCC " + JSON.stringify(channel, null, 2));
