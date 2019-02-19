817bacd527459816ca480d19872dd2d7eb7a524e
dodv <dodv212@gmail.com>
2019-02-19 15:53:59 +0700

    add mosca

---
+++ mosca.js
@@ -0,0 +1,46 @@
+const mosca = require('mosca');
+
+function start() {
+    const mongoDbUrl = 'mongodb://localhost:27017/mqtt';
+    const mqttPort = 1883;
+    const wsPort = 8000;
+
+    const moscaSettings = {
+        port: mqttPort,
+        backend: {
+            type: 'mongo',
+            url: mongoDbUrl,
+            pubsubCollection: 'ascoltatori',
+            mongo: {}
+        },
+        http: {
+            port: wsPort,
+            bundle: true,
+            static: './'
+        }
+    };
+
+    const server = new mosca.Server(moscaSettings);
+
+    // Client connects
+    server.on('clientConnected', (client) => {
+        console.log(`Client "${client.id}" connected.`);
+    });
+
+    // Client disconnects
+    server.on('clientDisconnected', (client) => {
+    });
+
+    // Message recieved
+    server.on('published', (packet, client) => {
+    });
+
+    // Server started
+    server.on('ready', () => {
+        console.log(`Mosca server running with MQTT on port ${mqttPort} and WebSockets on port ${wsPort}.`);
+    });
+
+    return server;
+};
+
+const server = start();
